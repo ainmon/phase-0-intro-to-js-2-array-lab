cb689f777b76d754aab237f7bdf4e54a2d896d73
Adam <inmon.adam@yahoo.com>
2021-08-15 21:01:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+// Write your solution here!//
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+   const cats = ["Milo", "Otis", "Garfield"];
+   const copyCat = [...cats, "Broom"];
+   return copyCat;
+}
+
+function prependCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const copyCat1 = ["Arnold", ...cats];
+    return copyCat1;
+}
+
+function removeLastCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const catsCopy = cats.slice(0, cats.length -1 );
+    return catsCopy;
+}
+
+function removeFirstCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const catsCopy = cats.slice(1);
+    return catsCopy;
+}
